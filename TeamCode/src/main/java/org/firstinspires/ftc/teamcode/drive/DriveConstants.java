b503da758349e17913ff33885a9a60b900d943f7
MrStickyG <123777607+MrStickyG@users.noreply.github.com>
2023-02-01 23:14:14 -0500

    Update DriveConstants.java
    tuning different things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -87,7 +87,7 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
      
      */
-    public static double MAX_VEL = 52.56591367802781;
+    public static double MAX_VEL = 55;
     public static double MAX_ACCEL = 52.56591367802781;
     public static double MAX_ANG_VEL = Math.toRadians(231.67730769230766);
     public static double MAX_ANG_ACCEL = Math.toRadians(231.67730769230766);
